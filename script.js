f8084067027bb245e3f9903ed7788117319d0482
Nikolya <75427338+korhenon@users.noreply.github.com>
2024-03-10 11:34:11 +0300

    create notification about answer

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const splash = document.getElementById("splash")
 const form = document.getElementById("form")
+const answerSaved = document.getElementById("answer-saved")
 
 window.onload = () => {
     document.body.style.overflow = "hidden"
+    answerSaved.style.display = "none"
 }
 
 splash.onclick = () => {
@@ -12,10 +14,13 @@ splash.onclick = () => {
 
 form.onsubmit = async (e) => {
     e.preventDefault()
+    form.style.display = "none"
+    answerSaved.style.display = "block"
+
     const data = new FormData(form)
     const url = `https://docs.google.com/forms/u/0/d/e/1FAIpQLScy6HBb2Zx4jSLq4O2Vi647fUnxMdlK0P_fy-kdLuhrnomjyw/formResponse?entry.807227795=${data.get("alcohol")}&entry.618516051=${data.get("contests")}`
 
-    const res = await fetch(url, {
+    await fetch(url, {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
